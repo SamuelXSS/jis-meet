ad70a8140642e4b88576b429dad68aa001a5c6e8
SamuelXSS <samuelxses@unipam.edu.br>
2020-12-20 17:07:23 -0300

    backend: added socketio connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const routes = require('./routes.js');
-const app = express();
+const express = require('express')
+const socket = require('socket.io')
+const routes = require('./routes.js')
 const cors = require('cors')
-
-app.use(cors());
-require('dotenv').config()
 require('./database')
 
-app.listen(3000)
-app.use(express.json())
+const app = express()
+app.use(cors());
+const io = socket(app.listen(3000), { cors: { origin: '*' } })
+
 app.use(routes)
+app.use(express.json())
+
+io.on('connection', socket => {
+    console.log('new conn')
+})
